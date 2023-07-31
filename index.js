7ae3118b9ef11743f8e7f7f82996049b44cabad4
Jia-Jia Zhu <dodge_waking_0b@icloud.com>
2023-07-31 10:14:43 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Jia-Jia';
+
+let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase(`Welcome to Flatbook, ${currentUser}!`);
+
+let shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
